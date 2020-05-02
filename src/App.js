cb5bbca28ffc6bbe46723b47fc4bba61be26d9d9
Ville Kopio <ville.kopio@kodan.fi>
2020-05-02 17:57:41 +0300

    Init firebase and change yarn to npm.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFulch9S_hXrOKzo0nOV54CZh3vW11nog",
+  authDomain: "satakuntatalo-services.firebaseapp.com",
+  databaseURL: "https://satakuntatalo-services.firebaseio.com",
+  projectId: "satakuntatalo-services",
+  storageBucket: "satakuntatalo-services.appspot.com",
+  messagingSenderId: "513995870934",
+  appId: "1:513995870934:web:d02b227e44d2f24e"
+};
 
 function App() {
+  firebase.initializeApp(firebaseConfig);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
