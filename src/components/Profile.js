1eb3ba0e680b9d56e5c79f02dfbb64160e29e933
Ville Kopio <ville.kopio@kodan.fi>
2020-05-09 16:51:29 +0300

    Add initial test functionality for token based auth.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,21 +1,49 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "../utils/auth0";
 
 const Profile = () => {
-  const { loading, user } = useAuth0();
+  const [showResult, setShowResult] = useState(false);
+  const [apiMessage, setApiMessage] = useState("");
+  const { loading, user, getTokenSilently } = useAuth0();
 
   if (loading || !user) {
     return <div>Loading...</div>;
   }
 
+
+  const callApi = async () => {
+    try {
+      const token = await getTokenSilently();
+
+      const response = await fetch("http://localhost:5001/satakuntatalo-services/europe-west3/backend/authorized", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      console.log(response)
+      const responseData = await response.json();
+
+      console.log(responseData)
+      setShowResult(true);
+      setApiMessage(responseData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <Fragment>
+    <>
       <img src={user.picture} alt="Profile" />
 
       <h2>{user.name}</h2>
       <p>{user.email}</p>
       <code>{JSON.stringify(user, null, 2)}</code>
-    </Fragment>
+
+      <h1>External API</h1>
+      <button onClick={callApi}>Ping API</button>
+      {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
+    </>
   );
 };
 
