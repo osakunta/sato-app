efd846efa62b157dfe0d68e3141428a33afa8eef
Ville Kopio <ville.kopio@kodan.fi>
2020-05-17 19:42:06 +0300

    Fetch albums on page load.

--- frontend/src/components/pages/Gallery.js
+++ frontend/src/components/pages/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '../../utils/auth0';
 import galleryService from '../../services/gallery';
 
@@ -6,25 +6,25 @@ const Gallery = () => {
   const { getTokenSilently } = useAuth0();
   const [albums, setAlbums] = useState([]);
 
-  const callApi = async () => {
-    const token = await getTokenSilently();
-
-    try {
-      const fetchedAlbums = await galleryService.list(token);
-
-      setAlbums(fetchedAlbums);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   const listAlbums = albums.map((album) => (<li key={album.id}>{album.title}</li>));
+
+  useEffect(() => {
+    (async () => {
+      const token = await getTokenSilently();
+
+      try {
+        const fetchedAlbums = await galleryService.list(token);
+
+        setAlbums(fetchedAlbums);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
 
   return (
     <div>
       <h1>Gallery</h1>
-
-      <button type="button" onClick={callApi}>Get albums</button>
 
       <h2>Albums</h2>
 
